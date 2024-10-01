df7aacb13cc0c16bcff03099268e0d42074254c6
Paweł Brzozowski <pawel@brzoza.net>
2024-10-01 19:49:14 +0200

    Refactor List component to use RecipeContext for active recipe highlighting

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,24 @@
 import css from "./List.module.css";
 import { Button } from "../Button/Button";
+import { useContext } from "react";
+import { RecipeContext } from "../../context/RecipeContext";
 
 export function List({ recipes, onSelectRecipe }) {
+  const recipe = useContext(RecipeContext);
+  console.log(recipe.id);
+
   return (
     <nav className={css.nav}>
       <span className={css.title}>Lista przepisów:</span>
       <ul className={css.list}>
         {recipes.map(({ id, name }) => (
           <li key={id} className={css.item}>
-            <Button onClick={() => onSelectRecipe(id)}>{name}</Button>
+            <Button
+              onClick={() => onSelectRecipe(id)}
+              active={recipe.id === id && true}
+            >
+              {name}
+            </Button>
           </li>
         ))}
       </ul>
